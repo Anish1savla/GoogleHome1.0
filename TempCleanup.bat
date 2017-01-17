5c5c7ff7fafb0aa4c3c15cef4346143a94900656
anishsavla <anish.savla@philips.com>
2017-01-17 09:17:21 -0800

    Adding Tempcleanup.bat file

---
+++ TempCleanup.bat
@@ -0,0 +1,9 @@
+cd C:\Users\310235474\AppData\Local
+ rmdir /S /Q Temp
+
+del C:\Windows\Prefetch\*.* /Q
+
+del C:\Windows\Temp\*.* /Q
+
+del C:\Users\310235474\AppData\Roaming\Microsoft\Windows\Recent Items*.* /Q
+pause
