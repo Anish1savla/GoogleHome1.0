b8248bb061d269b1ff2e61f2bf610edbc86ccbe5
anishsavla <anish.savla@philips.com>
2017-01-23 15:46:47 -0800

    Changing Web Simulator invokation method

--- src/HueGoogleHome.java
+++ src/HueGoogleHome.java
@@ -6,9 +6,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.sikuli.script.FindFailed;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
 
 import com.philips.lighting.model.PHBridge;
 
@@ -31,10 +34,8 @@ public class HueGoogleHome
       System.out.println("Inside onConnected and about to start test");
       try
       {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         
-        HueGoogleHome.startTests(bridge, driver);
+        HueGoogleHome.startTests(bridge);
       }
       catch (FindFailed e)
       {
@@ -111,31 +112,67 @@ public class HueGoogleHome
     }
   }
   
-  private static void startTests(PHBridge bridge, WebDriver driver)
+  private static void startTests(PHBridge bridge)
     throws FindFailed, InterruptedException, IOException
   {
+	  
+	  System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+      WebDriver driver = new ChromeDriver();
+      
+      driver.manage().deleteAllCookies();
+      driver.get("https://developers.google.com/actions/tools/web-simulator");
+      
+      driver.manage().window().maximize();
+      System.out.println("Chrome Window Maximized");
+      Screen screen = new Screen();
+      TimeUnit.SECONDS.sleep(1);
+      
+      Pattern image1 = new Pattern("Start.PNG");
+      //wait(30);
+      
+      //wait(30);
+      screen.click(image1);
+      
+      String winHandleBefore = driver.getWindowHandle();
+      driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+      for (String winHandle : driver.getWindowHandles()) {
+        driver.switchTo().window(winHandle);
+      }
+      driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+      driver.findElement(By.xpath("//*[@id='Email']")).click();
+      driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+      driver.findElement(By.xpath("//*[@id='Email']")).sendKeys(new CharSequence[] { "HueGHAutomation@gmail.com" });
+      driver.manage().timeouts().implicitlyWait(1L, TimeUnit.SECONDS);
+      driver.findElement(By.id("next")).click();
+      driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+      driver.findElement(By.xpath("//*[@id='Passwd']")).sendKeys(new CharSequence[] { "HueAutomation" });
+      driver.findElement(By.xpath("//*[@id='PersistentCookie']")).click();
+      driver.findElement(By.id("signIn")).click();
+      
+      driver.switchTo().window(winHandleBefore);
+      
 	  
     System.out.println("Inside start Test");
     TestCases tc = new TestCases();
     
-    tc.turnonalllights(bridge, driver);
+    //tc.turnonalllights(bridge, driver);
     
     tc.turnoffalllights(bridge, driver);
     
-    tc.changeColorToRed(bridge, driver);
+    //tc.changeColorToRed(bridge, driver);
     
-    tc.changeColorGreen(bridge, driver);
+   // tc.changeColorGreen(bridge, driver);
     
     
-    tc.turnoffalllights(bridge, driver);
+    //tc.turnoffalllights(bridge, driver);
     
-    tc.turnONHueColorLamp1(bridge, driver);
+    //tc.turnONHueColorLamp1(bridge, driver);
     
-    tc.SetBrightnessTo100(bridge, driver);
+    //tc.SetBrightnessTo100(bridge, driver);
     
-    tc.turnoffalllights(bridge, driver);
+    //tc.turnoffalllights(bridge,s driver);
     
-    tc.turnoffalllights(bridge, driver);
+    //tc.turnoffalllights(bridge, driver);
    
     System.out.println("Calling HTML Report create now");
     
